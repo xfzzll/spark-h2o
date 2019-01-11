03d1e3d7586efc05c8a1ed7b6d64f17db9a1fab1
zengliang <zengliang@ctrustech.com>
2019-01-11 10:43:17 +0800

    first commit

---
+++ ctrust-example1/src/main/scala/com/ctrust/spark/SelfTest.scala
@@ -0,0 +1,18 @@
+package com.ctrust.spark
+
+/**
+  * @program: ctrust-examples
+  * @description: ${description}
+  * @author: 曾亮
+  * @create: 2019-01-10 18:10
+  **/
+object SelfTest extends App {
+
+    other =>
+
+    val num = "what are you doing"
+
+    println(other.num eq (this.num))
+    println(this.num)
+
+}
